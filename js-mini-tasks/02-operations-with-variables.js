6248271a20f38acf3479ccee9e6faf276d1bcb04
Mindaugas <dizainas@gmail.com>
2022-07-05 11:20:40 +0300

    variables

--- js-mini-tasks/02-operations-with-variables.js
+++ js-mini-tasks/02-operations-with-variables.js
@@ -1,5 +1,5 @@
 // Veiksmai su kintamaisiais
-
+console.clear();
 // Susumuoti visus skaičiaus tipo kintamuosius
 // Rezultatą išvesti į console
 let a = 1;
@@ -9,9 +9,27 @@ console.log(a + b + c);
 
 // Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
 // Rezultatą išvesti į console
+let string1 = "Sujungti";
+let string2 = "teksto";
+let string3 = "kintamuosius";
+console.log(string1 + " " + string2 + " " + string3);
 
 // Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
 // 1-2+3-4+5
 // Rezultatą išvesti į console
 
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+console.log(one - two + three - four + five);
+
 // Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas
+
+let stringA = "Sujungti";
+let stringB = "teksto";
+let stringC = "kintamuosius";
+// let stringD = " ";
+// let stringC = ","
+console.log(stringC + "," + " " + stringB + "," + " " + stringA);
